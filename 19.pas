43d06de213de850bd8feaa019408adf8b73cc971
Fileger <144237631+Fileger@users.noreply.github.com>
2023-09-26 16:20:49 +0300

    Update 19.pas

--- 19.pas
+++ 19.pas
@@ -1,15 +1,11 @@
-﻿Program Zad1;
-var A:array [1..N] of integer;
-i, p,z : real;
-Begin
-  p:=1;
-  i:=0;
-  z:=1;
-  read(A);
-  while i<=a do 
-    begin
-    writeln
-    a-=1;
-    end;
-  writeln(p);
+Program Zad1;
+var n, k : integer;
+begin
+  read(n);
+while n<>0 do
+        begin
+         k:=n mod 10;
+        write(k);
+         n:=n div 10;
+        end;
 end.
